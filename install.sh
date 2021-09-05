9e2c064569df4588b1475f50cd6c29ed9697c057
Dima Sivov <dsivov@gmail.com>
2021-09-05 12:11:08 +0000

    plugin for kernel Linux volumio 4.19.118-v7l+ #1311 SMP

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Install oled display"
+sudo cp mpd_oled /usr/local/bin
+sudo chmod +x /usr/local/bin/mpd_oled
+sudo cp mpd_oled.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable mpd_oled
+sudo systemctl start mpd_oled
+echo "Install driver"
+sudo cp snd-soc-ddplayer-dac.ko /lib/modules/4.19.118-v7l+/kernel/sound/soc/bcm/snd-soc-ddplayer-dac.ko
+sudo cp ddplayer-dac.dtbo /boot/overlays/ddplayer-dac.dtbo
+echo "Install dacs list into webui"
+cp dacs.json /volumio/app/plugins/system_controller/i2s_dacs/dacs.json
