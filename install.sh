df91e2e3607fbcd6b5db3937cf3f8e2023677201
Dima Sivov <dsivov@gmail.com>
2021-09-13 12:09:45 +0000

    ichanging back to device 2

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ sudo systemctl daemon-reload
 sudo systemctl enable mpd_oled
 sudo systemctl start mpd_oled
 echo "Config overlay"
-sudo cp /boot/config.txt /boot/config.txt2
-sudo cp config.txt /boot/
+#sudo cp /boot/config.txt /boot/config.txt2
+#sudo cp config.txt /boot/
 echo "Install driver"
 sudo cp snd-soc-ddplayer-dac.ko /lib/modules/4.19.118-v7l+/kernel/sound/soc/bcm/snd-soc-ddplayer-dac.ko
 sudo cp ddplayer-dac.dtbo /boot/overlays/ddplayer-dac.dtbo
 echo "Install dacs list into webui"
 cp dacs.json /volumio/app/plugins/system_controller/i2s_dacs/dacs.json
 cp cards.json /volumio/app/plugins/audio_interface/alsa_controller/cards.json
-cp mpd.conf.tmpl /volumio/app/plugins/music_service/mpd/mpd.conf.tmpl
+#cp mpd.conf.tmpl /volumio/app/plugins/music_service/mpd/mpd.conf.tmpl
